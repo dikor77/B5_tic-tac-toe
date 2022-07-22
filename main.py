dad00545a3118b14d929a245fa2c8c2754ddeca5
DKovyazin <dakovyazin@gmail.com>
2022-07-22 16:00:40 +0600

    user input add function

--- main.py
+++ main.py
@@ -9,16 +9,18 @@ def fprint(field):
         print(i, ' '.join(field[i]))
 
 
-
-
+def user_input():
+    user_cross = []
+    while True:
+        user_cross = list(map(int, list(input("Ваш ход, поставьте крестик, формат (x y)").split())))
+        if len(user_cross) == 2 and all(list(map(lambda x: x in range(N), user_cross))):
+            print("Ваш выбор: ", user_cross)
+            return user_cross
+        else:
+            print("Ваш ввод не распознан, попробуй еще")
+        
 #начало
 fprint(field)
 
+user_cross = user_input()
 
-while True:
-    user_cross = list(map(int, list(input("Ваш ход, поставьте крестик, формат (x y)").split())))
-    if len(user_cross) == 2 and all(list(map(lambda x: x in range(N), user_cross))):
-        print("Ваш выбор: ", user_cross)
-        break;
-    else:
-        print("Ваш ввод не распознан, попробуй еще")
